50799a67c453486c08eeb3f5eefaf3d94b0ab77c
Unknown <carlosdz.centi@gmail.com>
2020-06-21 01:16:11 -0500

    Add project

---
+++ Exercises/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Exercises
+//
+//  Created by Carlos Diaz on 18/06/20.
+//  Copyright © 2020 Carlos Diaz. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
